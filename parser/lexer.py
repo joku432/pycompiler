37af9d1afd76bae3e859b0babe81e23da238f3d6
joku432 <poistajalol@hotmail.com>
2021-02-20 11:15:37 +0200

    something

--- parser/lexer.py
+++ parser/lexer.py
@@ -1,1 +1,25 @@
+import ctoken
 
+class Lexer:
+    def __init__(self, filename):
+        self.filename = filename
+        self.tokens   = []
+        self.count    = 0
+
+    def lex(self):
+        with open(self.filaname, "r") as f:
+            for line in f:
+                for c in line:
+                    if c.isalpha():
+                        self._make_ident()
+                    elif c.isdigit():
+                        self._make_number()
+                        
+    def _make_ident(self):
+        pass
+
+    def _make_number(self):
+        pass        
+
+     
+        
